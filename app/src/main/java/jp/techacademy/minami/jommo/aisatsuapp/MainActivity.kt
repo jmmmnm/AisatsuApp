a00f97e3f29e55bdb908378e950eb3fa9ab3f410
jmmmnm <jmmmnm@gmail.com>
2019-10-06 16:35:26 +0900

    ボタンを配置して、時刻取得

--- app/src/main/java/jp/techacademy/minami/jommo/aisatsuapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/minami/jommo/aisatsuapp/MainActivity.kt
@@ -2,11 +2,33 @@ package jp.techacademy.minami.jommo.aisatsuapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.app.TimePickerDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
     }
+
+    override fun onClick(v: View) {
+        showTimePickerDialog()
+    }
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+                this,
+                TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                    Log.d("UI_PARTS", "$hour:$minute")
+
+                },
+                13, 0, true)
+        timePickerDialog.show()
+    }
+
 }
